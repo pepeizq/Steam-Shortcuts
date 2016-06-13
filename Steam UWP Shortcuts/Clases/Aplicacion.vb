8a33d5ed006d03a1b1678d992fcaf29e9f6b57f2
pepei <pepei@DESKTOP-4NFJ4L7>
2016-06-13 16:11:14 +0200

    Add project files.

---
+++ Steam UWP Shortcuts/Clases/Aplicacion.vb
@@ -0,0 +1,31 @@
+﻿Public Class Aplicacion
+
+    Private _Nombre As String
+    Private _AccesoDirecto As String
+    Private _Imagen As String
+
+    Public ReadOnly Property Nombre() As String
+        Get
+            Return _Nombre
+        End Get
+    End Property
+
+    Public ReadOnly Property AccesoDirecto() As String
+        Get
+            Return _AccesoDirecto
+        End Get
+    End Property
+
+    Public ReadOnly Property Imagen() As String
+        Get
+            Return _Imagen
+        End Get
+    End Property
+
+    Public Sub New(ByVal nombre As String, ByVal accesodirecto As String, ByVal imagen As String)
+        _Nombre = nombre
+        _AccesoDirecto = accesodirecto
+        _Imagen = imagen
+    End Sub
+
+End Class
---
+++ Steam UWP Shortcuts/Modulos/FicherosINI.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class FicherosINI
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Shared Function GetPrivateProfileString(ByVal lpAppName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As StringBuilder, ByVal nSize As Integer, ByVal lpFileName As String) As Integer
+    End Function
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Shared Function WritePrivateProfileString(ByVal lpAppName As String, ByVal lpKeyName As String, ByVal lpString As String, ByVal lpFileName As String) As Boolean
+    End Function
+
+    Public Shared Function Leer(ByVal File As String, ByVal Section As String, ByVal Key As String) As String
+
+        Dim sb As New StringBuilder(10000)
+
+        GetPrivateProfileString(Section, Key, "", sb, sb.Capacity, File)
+
+        Return sb.ToString
+    End Function
+
+    Public Shared Sub Escribir(ByVal File As String, ByVal Section As String, ByVal Key As String, ByVal Value As String)
+
+        Try
+            WritePrivateProfileString(Section, Key, Value, File)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
